43a8e3b1c7f4bf57651bb716f3f81eeaaab47d6a
Sofie <sofiefreyr@gmail.com>
2022-04-26 08:26:28 +0300

    Day_16

--- src/dailyChallenges/Day_13.scala
+++ src/dailyChallenges/Day_13.scala
@@ -1,7 +1,5 @@
 package dailyChallenges
 
-import javafx.scene.layout.StackPane
-
 //Implement a class for a stack that supports all the regular functions (push, pop)
 // and an additional function of max() which returns the maximum element in the stack (return None if the stack is empty).
 // Each method should run in constant time.
